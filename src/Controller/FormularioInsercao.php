a4ada2a00b1262760114d7c4ff1fd4ae5bada06e
rezzgabriel <rezgabriel@outlook.com>
2021-10-10 22:29:30 -0300

    Validando senha

---
+++ src/Controller/FormularioInsercao.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Alura\Cursos\Controller;
+
+
+class FormularioInsercao extends ControllerComHtml implements InterfaceControladorRequisicao
+{
+    public function processaRequisicao(): void
+    {
+        echo $this->renderizaHtml('cursos/formulario.php', [
+            $titulo = 'Novo curso'
+        ]);
+
+    }
+}
